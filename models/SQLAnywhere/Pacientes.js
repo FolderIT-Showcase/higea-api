25366892b8deefdc01c2e293326bd946efb4862f
Miguel Bustos <mbustos@folderit.net>
2017-05-31 17:22:55 -0300

    Extendido esquema de Pacientes (preparacion para POST)

--- models/SQLAnywhere/Pacientes.js
+++ models/SQLAnywhere/Pacientes.js
@@ -6,7 +6,8 @@ var schema = {
     },
     plan_os_id_1: {
         type: "Number",
-        label: "ID Plan de OS 1"
+        label: "ID Plan de OS 1",
+        required: true
     },
     plan_os_id_2: {
         type: "Number",
@@ -15,6 +16,43 @@ var schema = {
     plan_os_id_3: {
         type: "Number",
         label: "ID Plan de OS 3"
+    },
+    pais_id: {
+        type: "Number",
+        label: "País"
+    },
+    provincia_id: {
+        type: "Number",
+        label: "Provincia"
+    },
+    localidad_id: {
+        type: "Number",
+        label: "Localidad",
+        required: true
+    },
+    estado_civil_id: {
+        type: "Number",
+        label: "Estado Civil"
+    },
+    persona_apellido: {
+        type: "String",
+        label: "Apellido",
+        required: true
+    },
+    persona_nombres: {
+        type: "String",
+        label: "Nombres",
+        required: true
+    },
+    persona_fecha_nacimiento: {
+        type: "Date",
+        label: "Fecha de Nacimiento"
+    },
+    persona_sexo: {
+        type: "String",
+        label: "Género",
+        enum: ["S", "N"],
+        default: "N"
     },
     persona_documento_nro: {
         type: "String",
@@ -31,6 +69,26 @@ var schema = {
     persona_telefono_lab_nro: {
         type: "String",
         label: "Teléfono Laboral"
+    },
+    persona_calle_nro: {
+        type: "String",
+        label: "Nro. de Calle"
+    },
+    persona_departamento_nro: {
+        type: "String",
+        label: "Nro. de Depto."
+    },
+    persona_piso_nro: {
+        type: "String",
+        label: "Nro. de Piso"
+    },
+    persona_email: {
+        type: "String",
+        label: "Email"
+    },
+    paciente_calle_texto: {
+        type: "String",
+        label: "Domicilio"
     },
     paciente_os_afiliado1_nro: {
         type: "String",
