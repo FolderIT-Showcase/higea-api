e68d1fcb8227b8bb362cae87991ccbe2e4130e20
Miguel Bustos <mbustos@folderit.net>
2017-05-31 11:55:06 -0300

    Agregado esquema de tabla TipoTurnoFac

---
+++ models/SQLAnywhere/TipoTurnoFac.js
@@ -0,0 +1,13 @@
+var schema = {
+    tipo_turno_fac_id: {
+        type: "Number",
+        label: "ID Tipo de Turno",
+        identity: true
+    },
+    tipo_turno_fac_nombre: {
+        type: "String",
+        label: "Nombre"
+    }
+};
+
+module.exports = schema;
